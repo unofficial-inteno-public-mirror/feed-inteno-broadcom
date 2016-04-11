6d49ef6d3638dff0119c5a54155b2c27d7ef6b45
Jonas Höglund <jonas.hoglund@embeddednation.com>
2016-04-11 17:20:38 +0200

    Update feed [ feed_inteno_broadcom ] package [ bcmkernel-3.4 ]
    -------------------------------------------------------------------------------
    * ddf18cc Add dsl firmwares to bcmkernel
    -------------------------------------------------------------------------------
    commit ddf18cc6b4f7bd4e8e9286c83484bb8bd1d876dc
    Author: Jonas Höglund <jonas.hoglund@embeddednation.com>
    Date: 2016-04-11 16:28:15 +0200
    
        Add dsl firmwares to bcmkernel
    
    Base directory -> /
     bcm963xx/dsl/963138BGWV/a_adsl_phy.bin | Bin 0 -> 1173884 bytes
     bcm963xx/dsl/963268GWV/a_adsl_phy.bin  | Bin 0 -> 1022524 bytes
     bcm963xx/dsl/963268GWV/b_adsl_phy.bin  | Bin 0 -> 1009640 bytes
     bcm963xx/dsl/963381GWV/a_adsl_phy.bin  | Bin 0 -> 1017588 bytes
     bcm963xx/dsl/963381GWV/b_adsl_phy.bin  | Bin 0 -> 1003268 bytes
     bcm963xx/dsl/96362GWV/a_adsl_phy.bin   | Bin 0 -> 602772 bytes
     bcm963xx/dsl/96362GWV/b_adsl_phy.bin   | Bin 0 -> 587720 bytes
     7 files changed, 0 insertions(+), 0 deletions(-)
    -------------------------------------------------------------------------------

--- bcmkernel/416050.mk
+++ bcmkernel/416050.mk
@@ -8,7 +8,7 @@
 # Update this based on the Broadcom SDK version, 4.16L.05 -> 416050
 BRCM_SDK_VERSION:=416050
 
-PKG_SOURCE_VERSION:=2fd7caba159a9532b3428db346de8de2904cdc5d
+PKG_SOURCE_VERSION:=ddf18cc6b4f7bd4e8e9286c83484bb8bd1d876dc
 
 ifneq ($(CONFIG_BCM_OPEN),y)
 PKG_NAME:=bcmkernel-3.4
