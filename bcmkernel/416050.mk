e50d8af3c3ffc84d0e6cdcba432a3a40389970cb
Kenneth Johansson <kenneth@southpole.se>
2016-09-28 18:26:51 +0200

    Update feed [ feed_inteno_broadcom ] package [ bcmkernel-3.4 ]
    -------------------------------------------------------------------------------
    * a72c9d8 dsl: change annex B to set B/J in hardware.
    -------------------------------------------------------------------------------
    commit a72c9d82546e0d187bcebacc7dad064f6e36dec0
    Author: Kenneth Johansson <kenneth@southpole.se>
    Date: 2016-09-28 18:26:19 +0200
    
        dsl: change annex B to set B/J in hardware.
    
    Base directory -> /
     bcm963xx/bcmdrivers/broadcom/char/adsl/impl1/BcmAdslCore.c | 3 +--
     1 file changed, 1 insertion(+), 2 deletions(-)
    -------------------------------------------------------------------------------

--- bcmkernel/416050.mk
+++ bcmkernel/416050.mk
@@ -8,7 +8,7 @@
 # Update this based on the Broadcom SDK version, 4.16L.05 -> 416050
 BRCM_SDK_VERSION:=416050
 
-PKG_SOURCE_VERSION:=94602ec0d0121847299b73e4df0fbd5e1863feda
+PKG_SOURCE_VERSION:=a72c9d82546e0d187bcebacc7dad064f6e36dec0
 
 ifneq ($(CONFIG_BCM_OPEN),y)
 PKG_NAME:=bcmkernel-3.4
