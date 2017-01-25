df7cc264614c469dc62689cb4ea654cb6a89bcf0
Kenneth Johansson <kenneth@southpole.se>
2017-01-25 15:29:42 +0100

    Update feed [ feed_inteno_broadcom ] package [ bcmkernel-3.4 ]
    -------------------------------------------------------------------------------
    * 30220a8 Do not ignore MTU for JUMBO when accelerated.
    -------------------------------------------------------------------------------
    commit 30220a803bbfd27e412190b90a4382187af9b83a
    Author: Kenneth Johansson <kenneth@southpole.se>
    Date: 2017-01-25 15:26:37 +0100
    
        Do not ignore MTU for JUMBO when accelerated.
    
        this prevented compile if it was unset.
    
    Base directory -> /
     bcm963xx/hostTools/scripts/defconfig-bcm.template | 2 ++
     1 file changed, 2 insertions(+)
    -------------------------------------------------------------------------------

--- bcmkernel/416050.mk
+++ bcmkernel/416050.mk
@@ -8,7 +8,7 @@
 # Update this based on the Broadcom SDK version, 4.16L.05 -> 416050
 BRCM_SDK_VERSION:=416050
 
-PKG_SOURCE_VERSION:=ac98d0f9fdebf96baedcdeac6c61f7fb196b3b07
+PKG_SOURCE_VERSION:=30220a803bbfd27e412190b90a4382187af9b83a
 
 ifneq ($(CONFIG_BCM_OPEN),y)
 PKG_NAME:=bcmkernel-3.4
