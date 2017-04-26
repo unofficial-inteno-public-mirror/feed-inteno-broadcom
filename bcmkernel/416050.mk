cd0e9a4f48b51c5b8e792a3a0543f70e15891088
Sukru Senli <sukru.senli@inteno.se>
2017-04-26 09:50:16 +0200

    bcmkernel updated

--- bcmkernel/416050.mk
+++ bcmkernel/416050.mk
@@ -8,7 +8,7 @@
 # Update this based on the Broadcom SDK version, 4.16L.05 -> 416050
 BRCM_SDK_VERSION:=416050
 
-PKG_SOURCE_VERSION:=e25fff1c80c41a7cb365faaee23ce528d23399c9
+PKG_SOURCE_VERSION:=428bfaaeea9c46387b81a24a2fe5261c50d1e676
 
 ifneq ($(CONFIG_BCM_OPEN),y)
 PKG_NAME:=bcmkernel-3.4
